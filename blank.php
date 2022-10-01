6fe05c4d88f3081bdcd237f78212d808fdd71885
Muhammad Asif <alexasifhhossain@gmail.com>
2022-10-01 20:15:44 +0600

    blank content created

---
+++ blank.php
@@ -0,0 +1,33 @@
+<?php include 'includes/head.php'; ?>
+<?php include 'includes/preloader.php'; ?>
+<?php include 'includes/navbar.php'; ?>
+<?php include 'includes/sidebar.php'; ?>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Dashboard</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Dashboard</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- dashboard main content Start -->
+    <section>
+      This is Blank Page<br>
+      Write your code here
+    </section>
+    <!-- main content End -->
+  </div>
+
+
+<?php include 'includes/footer.php'; ?>
