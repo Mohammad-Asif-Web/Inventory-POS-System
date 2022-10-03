039e6077d519a3c96434779a1955b6dcc28c1219
Muhammad Asif <alexasifhhossain@gmail.com>
2022-10-03 13:50:31 +0600

    forgot password completed

---
+++ forgot-password.php
@@ -0,0 +1,85 @@
+<?php
+include 'functions/config.php';
+if(isset($_POST['submit'])){
+  $email = $_POST['email'];
+  // echo $email;
+
+  $sql = "SELECT * FROM tbl_user WHERE user_email = ? ";
+
+  $query = $pdo->prepare($sql);
+  $query->bindParam(1, $email);
+  $query->execute();
+
+  $result = $query->fetch(PDO::FETCH_ASSOC);
+
+  if($result){
+    session_start();
+    $_SESSION['userID'] = $result['user_id'];
+    $_SESSION['userName'] = $result['username'];
+    header("location: recover-password.php");
+
+  } else{
+    echo "<div class='alert alert-warning'>Email Doesn't Match</div>";
+  }
+
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>POS | Forgot Password</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="assets/css/adminlte.min.css">
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <div class="card card-outline card-primary">
+    <div class="card-header text-center">
+      <a href="index.php" class="h1"><b>Inventory</b>POS</a>
+    </div>
+    <div class="card-body">
+      <p class="login-box-msg">You forgot your password? Here you can easily retrieve a new password.</p>
+      <form action="<?php $_SERVER['PHP_SELF']?>" method="post">
+        <div class="input-group mb-3">
+          <input type="email" name="email" class="form-control" placeholder="Email" required>
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <button type="submit" name="submit" class="btn btn-primary btn-block">Request new password</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+      <p class="mt-3 mb-1">
+        <a href="index.php">Sign in</a>
+      </p>
+    </div>
+    <!-- /.login-card-body -->
+  </div>
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery -->
+<script src="../../plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../../dist/js/adminlte.min.js"></script>
+</body>
+</html>
