4d5702b513db3fcc6c7dda8e6661ae5cf0927365
Muhammad Asif <alexasifhhossain@gmail.com>
2022-10-05 14:04:56 +0600

    script file moved to head

--- includes/head.php
+++ includes/head.php
@@ -4,12 +4,23 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>AdminLTE 3 | Dashboard 2</title>
-
+    <!-- jQuery -->
+  <script src="plugins/jquery/jquery.min.js"></script>
+  <!-- Bootstrap -->
+  <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="assets/js/adminlte.js"></script>
+  <!-- AdminLTE for demo purposes -->
+  <script src="assets/js/demo.js"></script>
+  <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+  <script src="assets/js/pages/dashboard2.js"></script>
+    <!-- sweet alert 2 CDN -->
+  <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome Icons -->
   <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
-
   <!-- Theme style -->
   <link rel="stylesheet" href="assets/css/adminlte.min.css">
+
 </head>
