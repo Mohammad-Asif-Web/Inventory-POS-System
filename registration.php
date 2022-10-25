99e61092789a5c428a9656bf4aabdd1c1caebfd6
Muhammad Asif <alexasifhhossain@gmail.com>
2022-10-25 09:20:51 +0600

    registration page completed

--- registration.php
+++ registration.php
@@ -8,7 +8,77 @@ if($_SESSION['userEmail'] == '' || $_SESSION['userRole'] == 'User'){
 include 'includes/head.php'; 
 include 'includes/preloader.php'; 
 include 'includes/navbar.php'; 
-include 'includes/sidebar.php'; 
+include 'includes/sidebar.php';
+
+// Delete Data 
+// error_reporting(0);
+if(isset($_GET['id'])){
+    $del_id = $_GET['id'];
+    $sqlDel = "DELETE FROM tbl_user WHERE user_id = :del_id";
+    $delete = $pdo->prepare($sqlDel);
+    $delete->bindParam(':del_id', $del_id);
+    if($delete->execute()){
+        // header("location: registration.php");
+        echo '<script type="text/javascript">
+        jQuery(function validation(){
+            Swal.fire({
+            title: "Good Job! '.strtoupper($_SESSION['userName']).'",
+            icon: "success",
+            html:
+                "<h1>Account is Deleted !</h1>",
+            confirmButtonText:
+                "Ok",
+            })
+        });
+        </script>';
+    } 
+}
+
+// Insert data
+if(isset($_POST['submit'])){
+    // echo 'this is registration page';
+    $name = $_POST['username'];
+    $email = $_POST['email'];
+    $password = md5($_POST['pass']);
+    $role = $_POST['role'];
+
+    $sqlEmail = "SELECT user_email FROM tbl_user WHERE user_email = :email ";
+    $queryEmail = $pdo->prepare($sqlEmail);
+    $queryEmail->bindParam(':email', $email);
+    $queryEmail->execute();
+
+    if($queryEmail->rowCount() > 0){
+        echo 'Email Already Exists';
+    } else {
+        $sqlInsert = "INSERT INTO tbl_user (username, user_email, password, role) VALUES(:username, :email, :pass, :role)";
+    
+        $queryInsert = $pdo->prepare($sqlInsert);
+        $queryInsert->bindParam(':username', $name);
+        $queryInsert->bindParam(':email', $email);
+        $queryInsert->bindParam(':pass', $password);
+        $queryInsert->bindParam(':role', $role);
+    
+        $result = $queryInsert->execute();
+        if($result){
+            echo '<script type="text/javascript">
+            jQuery(function validation(){
+                Swal.fire({
+                title: "Good Job! '.strtoupper($_SESSION['userName']).'",
+                icon: "success",
+                html:
+                    "<h1>New Account Created !</h1>",
+                confirmButtonText:
+                    "Ok",
+                })
+            });
+            </script>';
+        } else {
+            echo "<p class='alert alert-danger text-uppercase'>Query Failed: data not inserted </p>";
+    
+        }
+    }
+}
+
 ?>
 
   <!-- Content Wrapper. Contains page content -->
@@ -36,14 +106,14 @@ include 'includes/sidebar.php';
             <div class="col-md-4">
                 <div class="card card-info">
                     <div class="card-header">
-                        <h3 class="card-title">Register a New Member</h3>
+                        <h3 class="card-title">Register a New User</h3>
                     </div>
-                    <form action="" method="post">
+                    <form action="<?php $_SERVER['PHP_SELF']?>" method="post">
                         <!-- card body -->
-                        <div class="card-body">
+                        <div class="card-body pb-0">
                             <div class="form-group">
                                 <label for="username">Name</label>
-                                <input type="text" class="form-control" name="username" id="username" placeholder="Your name" required>
+                                <input type="text" class="form-control" name="username" id="username" placeholder="Your name" >
                             </div>
                             <div class="form-group">
                                 <label for="email">Email Address</label>
@@ -63,7 +133,7 @@ include 'includes/sidebar.php';
                             </div>
                         </div>
                         <!-- card footer -->
-                        <div class="card-footer">
+                        <div class="card-footer pt-0 bg-dark">
                             <button type="submit" name="submit" class="btn btn-info">Submit</button>
                         </div>
                     </form>
@@ -73,6 +143,7 @@ include 'includes/sidebar.php';
                 <div class="card card-info">
                     <table class="table table-bordered">
                         <div class="card-header">
+                            <h3 class="card-title text-center">Records of Users</h3>
                             <thead>
                                 <tr>
                                 <th style="width: 10px">#</th>
@@ -85,17 +156,18 @@ include 'includes/sidebar.php';
                             </thead>
                         </div>
                             <!-- card body -->
-                        <div class="card-body">
+                        <div class="card-body p-0">
                             <tbody>
                             <?php
                             $sql = "SELECT * FROM tbl_user ORDER BY user_id DESC ";
                             $query = $pdo->prepare($sql);
                             $query->execute();
+                            $sl = 1;
                             
                             while($row = $query->fetch(PDO::FETCH_OBJ)){
                             ?>
                             <tr>
-                                <td><?php echo $row->user_id ?></td>
+                                <td><?php echo $sl++ ?></td>
                                 <td><?php echo $row->username ?></td>
                                 <td>
                                 <?php echo $row->user_email ?>
@@ -103,7 +175,8 @@ include 'includes/sidebar.php';
                                 <td><?php echo $row->password ?></td>
                                 <td><?php echo $row->role ?></td>
                                 <td>
-                                    <a href="registration.php?id="><button class="btn btn-sm btn-danger">button</button></a>
+                                    <a href="registration.php?id=<?php echo $row->user_id ?>"><button class="btn btn-sm btn-danger"
+                                    name="delete">button</button></a>
                                 </td>
                             </tr>
                             <?php
@@ -119,6 +192,5 @@ include 'includes/sidebar.php';
     </section>
     <!-- main content End -->
   </div>
-
 
 <?php include 'includes/footer.php'; ?>
